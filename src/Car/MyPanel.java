911932e879f01a9f59d5d4f55e37599fc0917423
Yaroslav <asgen-yaroslav@mail.ru>
2022-07-11 23:08:33 +0300

    added Car

---
+++ src/Car/MyPanel.java
@@ -0,0 +1,19 @@
+package Car;
+
+import java.awt.*;
+import javax.swing.JPanel;
+
+public class MyPanel extends JPanel {
+
+	MyPanel() {
+		this.setPreferredSize(new Dimension(600,600));
+		
+	}
+	
+	public void paint(Graphics g) {
+		
+		Graphics2D g2D = (Graphics2D) g;
+		
+		
+	}
+}
