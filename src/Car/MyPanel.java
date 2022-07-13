ba684e85327124e096e749b9b62cec4cb193df7d
Yaroslav <asgen-yaroslav@mail.ru>
2022-07-13 18:37:47 +0300

    added wheels

--- src/Car/MyPanel.java
+++ src/Car/MyPanel.java
@@ -1,7 +1,8 @@
 package Car;
 
 import java.awt.*;
-import javax.swing.JPanel;
+import java.awt.geom.*;
+import javax.swing.*;
 
 public class MyPanel extends JPanel {
 
@@ -13,6 +14,25 @@ public class MyPanel extends JPanel {
 	public void paint(Graphics g) {
 		
 		Graphics2D g2D = (Graphics2D) g;
+		
+		//Background
+		g2D.setColor(Color.LIGHT_GRAY);;
+		g2D.fillRect(0, 0, 600,600);
+		
+		//Wheels
+		g2D.setColor(Color.white);
+		g2D.fillOval(125, 460, 80, 80);
+		g2D.fillOval(395, 460, 80, 80);
+		g2D.setColor(Color.black);
+		g2D.setStroke(new BasicStroke(25));
+		g2D.drawOval(125, 460, 80, 80);
+		g2D.drawOval(395, 460, 80, 80);
+		
+		g2D.setStroke(new BasicStroke());
+		Area car = new Area(new Line2D.Double(220, 500, 380, 500));
+		Area temp = new Area(new Arc2D.Double(110, 445, 110, 55, 0, 180, Arc2D.CHORD));
+		
+		g2D.draw(temp);
 		
 		
 	}
